00df6450772abba5357129b073d1f8832f716d3a
anne <richardson.ae@gmail.com>
2017-10-25 15:36:26 -0500

    Build out form

--- app.rb
+++ app.rb
@@ -1,3 +1,15 @@
 require 'sinatra'
-require "sinatra/reloader" if development?
+require 'sinatra/reloader' if development?
 require 'erb'
+
+
+get '/name_form' do
+  erb :name_form
+end
+
+post '/name_form' do
+  # pick up the name from the form
+  user_name = params[:name_from_form]
+  # send the data to the show_name view as the 'name' variable
+  erb :show_name, locals: { name: user_name }
+end
